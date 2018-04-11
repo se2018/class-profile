0cbd5c3500af6dac76bd01200923ca0a92a14b61
Andy Zhang <andzhng@gmail.com>
2018-04-11 00:40:10 -0400

    Add some documentation, fix formatting

--- analyses/Bucket.py
+++ analyses/Bucket.py
@@ -3,25 +3,26 @@
 Specifically, a lot of correlations rely on grouping specific data points,
 followed by seeing if any bucket is significantly greater than other buckets.
 """
-import json
 
 
 class Bucket:
 
-    def __init__(self, col_name, col_value, grades, salary):
+    def __init__(self, col_name, col_value, grades, salaries):
         self.col_name = col_name
         self.col_value = col_value
         self.grades = grades   # sorted by time (i.e. term)
-        self.salary = salary   # sorted by time (i.e. term)
+        self.salaries = salaries   # sorted by time (i.e. term)
 
     def summary(self):
-        print 'Grade Summary for ', self.col_name, self.col_value
-        for d in self.grades:
-            print json.dumps(d.summary(), indent=True)
-
-        print 'Salary Summary for ', self.col_name, self.col_value
-        for d in self.salary:
-            print json.dumps(d.summary(), indent=True)
+        s = {}
+        s['col_name'] = self.col_name
+        s['col_value'] = self.col_value
+        if self.size() < 3:
+            s['message'] = 'Only ' + str(self.size()) + ' entries, not enough points.'
+            return s
+        s['grades'] = [d.summary() for d in self.grades]
+        s['salaries'] = [d.summary() for d in self.salaries]
+        return s
 
     def size(self):
-        return self.grades[0].count()  # Any distribution can work
+        return min(min(map(lambda x: x.count(), self.grades)), min(map(lambda x: x.count(), self.salaries)))
